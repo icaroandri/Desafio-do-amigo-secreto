fd56164b3213c5773229e346f758aa66bf71752d
icaro <icoerafa@hotmail.com>
2025-02-01 16:58:18 -0300

    segundo commit

--- app.js
+++ app.js
@@ -1,14 +1,39 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
 
+let ListaDosnomes = [];
+let novoNome = '';
 
-let     ListaDosnomes = [];
-let novoNome =  ('');
-
-function adicionarAmigo()  {
-let novoNome = document.querySelector('input-name');
-return novoNome; 
-    
+function adicionarAmigo() {
+    novoNome = document.getElementById('amigo').value;  // Pega o valor do input
+    if (novoNome === '') {
+        alert('Por favor, insira um nome.');  // Se o campo estiver vazio, alerta o usuário
+    } else {
+        ListaDosnomes.push(novoNome);  // Adiciona o nome à lista
+        console.log(ListaDosnomes); 
+        
+        // Mostra a lista atualizada no console
+    }
+    // Limpa o campo de input após adicionar o nome (opcional)
+    document.getElementById('amigo').value = ''; 
 }
 
-console.log(novoNome)
+// sortear nome
+
+
+function sortearAmigo() {
+
+    let tamFun = ListaDosnomes.length;
+    console.log(tamFun);
+    if (tamFun > 0 ){
+        let numeroSorteio = Math.floor(Math.random() * tamFun)
+        let nomeSorteado = ListaDosnomes[numeroSorteio]
+        console.log (nomeSorteado) ; 
+
+        document.getElementById('resultado').innerHTML = `Amigo sorteado: ${nomeSorteado}`
+
+    } else {
+        alert('A lista está vazia piá')
+    }
+    
+ }
 
