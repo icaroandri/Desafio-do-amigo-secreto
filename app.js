e53c4173f25aa4fd6b7e25140ad918f01655e09a
icaro <icoerafa@hotmail.com>
2025-02-01 15:57:42 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+
+
+let     ListaDosnomes = [];
+let novoNome =  ('');
+
+function adicionarAmigo()  {
+let novoNome = document.querySelector('input-name');
+return novoNome; 
+    
+}
+
+console.log(novoNome)
+
